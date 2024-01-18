c020e32a06a6ed760c1817cd7caa4e71b4b925e8
Shobik-maharjan <shobitmah32@gmail.com>
2024-01-18 18:18:39 +0545

    js

---
+++ index.js
@@ -0,0 +1,10 @@
+menuToggle.addEventListener("click", function () {
+  document.getElementById("menuToggle").classList.toggle("change");
+
+  let menus = document.getElementsByClassName("menus");
+  menus.toggle = "menus";
+});
+// function myFunction() {
+//   document.getElementById("menuToggle").classList.toggle("change");
+//   //   this.classList.toggle("change");
+// }
