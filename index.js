8c60c510e191bf1a9d3704bfb049e7daa801ced6
Shobik-maharjan <shobitmah32@gmail.com>
2024-01-19 16:31:47 +0545

    responsive

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 menuToggle.addEventListener("click", function () {
-  document.getElementById("menuToggle").classList.toggle("change");
-
-  let menus = document.getElementsByClassName("menus");
-  menus.toggle = "menus";
+  document.getElementById("bars").classList.toggle("change");
+  document.querySelector("#menus").classList.toggle("menus");
+  document.querySelector("#menu-bars").classList.toggle("displays");
 });
 // function myFunction() {
 //   document.getElementById("menuToggle").classList.toggle("change");
 //   //   this.classList.toggle("change");
 // }
+
+function selectImage(x) {
+  let one = 1;
+  let two = 2;
+  let three = 3;
+  if (x == one) {
+    one.document.querySelector(".product-image").src =
+      "./images/image-product-2.jpg";
+  }
+  if (x == two) {
+    two.document.querySelector(".product-image").src =
+      "./images/image-product-3.jpg";
+  }
+  if (x == three) {
+    three.document.querySelector(".product-image").src =
+      "./images/image-product-4.jpg";
+  }
+}
